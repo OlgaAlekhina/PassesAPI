f856650597d0569378b187104517fa9861c1e9cd
Olga Alekhina <89858498+OlgaAlekhina@users.noreply.github.com>
2022-07-01 22:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
